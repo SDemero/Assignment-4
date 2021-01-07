fb8c30e8ec5dc8f536985867fe4ca2d670e16fcd
Youness Elwiddi <ywiddi1020@gmail.com>
2021-01-07 16:33:07 -0500

    Update password.js

--- password.js
+++ password.js
@@ -3,6 +3,7 @@ let formselect = document.querySelector("form");
 formselect.addEventListener("submit", check);
 
 function check(){
+	console.log("test");
 	let password = document.getElementById("psw").value;
 	if(password.value !== "123456789")
         	alert("Submission Incorrect");
